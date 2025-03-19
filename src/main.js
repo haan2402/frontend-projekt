a69252d4de33482bc96f556c42e0ae37388b0ebf
Hanna Angeria <haan2402@student.miun.se>
2025-03-19 09:18:49 +0100

    gjort scss för att centrera innehåll, lagt till färgerna jag vill ha på sidan, och hämtat in kartan via leafletjs instruktioner

--- src/main.js
+++ src/main.js
@@ -1,1 +1,36 @@
 "use strict";
+
+/**
+ * Hämtar in karta från Leaflet, startar marker på Stockholm
+ */
+const map = L.map('map').setView([59.3293, 18.0686], 10);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+const marker = L.marker([59.3293, 18.0686]).addTo(map);
+
+/**
+ * Funktion som uppdaterar kartan efter sökning 
+ */
+function getMap() {
+    const searchInput = document.getElementById('search-place').value;
+    const mapUrl = `https://nominatim.openstreetmap.org/search.php?format=json&q=${searchInput}`;
+
+    fetch(mapUrl) 
+        .then(response => response.json())
+        .then(data => {
+            if(data.length > 0) {
+                const lat = data[0].lat;
+                const lon = data[0].lat;
+
+                map.setView([lat, lon], 10);
+                marker.setLatLng([lat, lon]);
+
+                getWeather(lat, lon);
+            }
+        })
+        .catch(error => console.error("Problem med nätverksanslutning", error));
+}

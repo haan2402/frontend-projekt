0c02fb812a26a452855f4cabc3e937bf750773a2
Hanna Angeria <haan2402@student.miun.se>
2025-03-19 12:11:38 +0100

    hämtat in väder beroende på koordinater, stylat mer med scss

--- src/main.js
+++ src/main.js
@@ -17,14 +17,14 @@ const marker = L.marker([59.3293, 18.0686]).addTo(map);
  */
 function getMap() {
     const searchInput = document.getElementById('search-place').value;
-    const mapUrl = `https://nominatim.openstreetmap.org/search.php?format=json&q=${searchInput}`;
+    const mapUrl = `https://nominatim.openstreetmap.org/search?format=json&q=${searchInput}`;
 
     fetch(mapUrl) 
         .then(response => response.json())
         .then(data => {
             if(data.length > 0) {
                 const lat = data[0].lat;
-                const lon = data[0].lat;
+                const lon = data[0].lon;
 
                 map.setView([lat, lon], 10);
                 marker.setLatLng([lat, lon]);
@@ -34,3 +34,35 @@ function getMap() {
         })
         .catch(error => console.error("Problem med nätverksanslutning", error));
 }
+
+/**
+ * Hämtar in väderinfo
+ */
+async function getWeather(lat, lon) {
+    try {
+        const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=rain_sum,uv_index_max&hourly=temperature_2m&current=temperature_2m&timezone=auto&forecast_days=1&wind_speed_unit=ms`);
+        if(!response.ok) {
+            throw new Error('Problem med nätverksanslutning')
+        }
+        const data = await response.json();
+        writeWeather(data);
+    } catch(error) {
+        console.error('Problem vid hämtning av väder', error);
+    }
+}
+
+/**
+ * Funktion för att hämta in väderdatan
+ */
+function writeWeather(data) {
+    document.getElementById('temp').innerHTML = `Temperatur: ${data.current.temperature_2m} C`;
+    document.getElementById('rain').innerHTML = `Nederbörd: ${data.daily.rain_sum[0]} mm`;
+    document.getElementById('uv').innerHTML = `UV-index: ${data.daily.uv_index_max[0]}`;
+}
+
+
+
+/**
+ * Eventlyssnare för sök-knappen
+ */
+document.getElementById('search').addEventListener('click', getMap);
